f01565e3e162c9136f3ae4bb2c7b78eccf5a7ed6
Benjamin Falcon <benjamin.falcon@virginiogomez.cl>
2021-12-21 14:13:13 -0300

    COMMIT FINAL

--- app/src/main/java/com/farmacia/app/MainActivity.java
+++ app/src/main/java/com/farmacia/app/MainActivity.java
@@ -1,26 +1,95 @@
 package com.farmacia.app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText etEmail;
+    TextInputEditText etPassword;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Farmacia");
+    }
 
-        myRef.setValue("Farmacia2");
-
+    private void initViews() {
+        mAuth = FirebaseAuth.getInstance();
+        etEmail = (TextInputEditText) findViewById(R.id.et_email_login);
+        etEmail = (TextInputEditText) findViewById(R.id.et_password_login);
 
     }
 
-}
+    public void clickAcceder(View view) {
+        //  if (TextUtils.isEmpty(etEmail.getText()) || TextUtils.isEmpty(etPassword.getText())){
+        //     Toast.makeText( this,"Debes rellenar todos los campos ", Toast.LENGTH_SHORT).show();
+        //    }else {
+      //  String email = etEmail.getText().toString();
+        //String password = etPassword.getText().toString();
+        Intent intentScreen = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intentScreen);
+    //    acceder(email, password);
+
+    }
+
+
+//}
+    private void acceder(String email, String  password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener( this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText( getApplicationContext(),"Accediste con exito", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText( getApplicationContext(),"No se ha podido acceder  ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
+
+    public void clickRecuperarPass(View view){
+
+        String email = etEmail.getText().toString();
+
+        if(TextUtils.isEmpty(etEmail.getText())) {
+            Toast.makeText( getApplicationContext(),"introduce un email", Toast.LENGTH_SHORT).show();
+
+        }else {
+            mAuth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText( getApplicationContext(),"Se le ah enviado un email para reestablecer su contraseña ", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+        }
+
+    }
+                }
+
+
---
+++ app/src/main/java/com/farmacia/app/MapsActivity.java
@@ -0,0 +1,63 @@
+package com.farmacia.app;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.farmacia.app.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+//-36.606552301593375, -72.10330409210579
+        // Add a marker in Sydney and move the camera
+        LatLng Chillan= new LatLng(-36.606552301593375,-72.10330409210579);
+        mMap.addMarker(new MarkerOptions().position(Chillan).title(" chillan"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Chillan));
+
+//-36.610362854116545, -72.10206441413456 (F.santos)
+        LatLng FarmaciaSts= new LatLng(-36.610362854116545,-72.10206441413456);
+        mMap.addMarker(new MarkerOptions().position(FarmaciaSts).title("Farmacia Santos, martes, 9:00–18:30 ,miércoles, 9:00–18:30,jueves, 9:00–18:30,viernes, 9:00–18:30,sábado (Navidad), Cerrado, Horario del día feriado,domingo, Cerrado,lunes, 9:00–18:30"));
+
+
+//-36.60760692013815, -72.09245137779658 (F scb)
+        LatLng FarmaciaSb= new LatLng(-36.60760692013815,-72.09245137779658);
+        mMap.addMarker(new MarkerOptions().position(FarmaciaSb).title("Farmacia Salcobrand, martes, 9:00–20:30,miércoles, 9:00–20:30,jueves, 9:00–20:30,viernes, 9:00–20:30,sábado (Navidad), 9:00–19:30, Los horarios pueden variar,domingo, Cerrado,lunes, 9:00–20:30"));
+//-36.60990334513379, -72.10142155599945
+        LatLng FarmaciaDrSimi= new LatLng(-36.60990334513379,-72.10142155599945);
+        mMap.addMarker(new MarkerOptions().position(FarmaciaDrSimi).title("FarmaciaDrSimi,LUNES A VIERNES DE 09:00 A 18:00\""));
+
+//-36.60966132343415, -72.10072442691968
+        LatLng FarmaciaAhumada= new LatLng(-36.60966132343415,-72.10072442691968);
+        mMap.addMarker(new MarkerOptions().position(FarmaciaAhumada).title("FarmaciaAhumada, LUNES A VIERNES DE 09:00 A 18:00\""));
+        //-36.609046028105446, -72.101773339236
+        LatLng FarmaciaCrusVerde= new LatLng(-36.609046028105446,-72.101773339236);
+        mMap.addMarker(new MarkerOptions().position(FarmaciaCrusVerde).title("FarmaciaCrusVerde, LUNES A VIERNES DE 09:00 A 18:00"));
+
+    }
+
+}
